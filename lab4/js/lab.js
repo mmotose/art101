2f4a5e865b84d5db33787e7548b14e950034be44
mmotose <129877604+mmotose@users.noreply.github.com>
2023-04-24 18:39:59 -0700

    Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,8 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - War - An implementation of the card game
+// Author: Miyuki Motose <mmotose@ucsc.edu>
+// Date: 4/24/2023
 
-// Constants
+// Pseudocode
+//Generate standard deck of cards (52 cards, 4 suits)
+  //“Shuffle”/Randomize the cards 
+  //Cut deck in half
+  //Give half the deck to Player A and the other half to Player B
+//Player A select 1 card from their half of the deck
+//Player B select 1 card from their half of the deck
+  //Compare the two cards
+    //If Player A has the higher number or face card than Player B then they win the match they get one point
+    //If Player B has the higher number or face card than Player A then they win the match they get one point
+//Discord the drawn cards
+//Repeat until cards runout 
+//Compare point scores, whoever has the most wins
+
 
 // Functions
 
