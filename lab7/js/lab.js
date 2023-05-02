b4c13e10515766e553e413041d1ad043fe13e12d
mmotose <129877604+mmotose@users.noreply.github.com>
2023-05-02 15:58:13 -0700

    Lab7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -2,3 +2,28 @@
 // Date: 5/2/2023
 
 // Constants
+
+function sortUsername(){
+    //prompt user for username
+    var userName = window.prompt("Please enter your name. I'm going to fix it.");
+    console.log("userName =", userName);
+    userName = userName.toLowerCase();
+
+    //make string into array
+    var nameArray = userName.split('');
+    console.log("nameArray =", nameArray);
+
+    //sort the array
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort =", nameArraySort);
+
+    //join array back to a string
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+
+    return nameSorted;
+}
+
+//Output
+document.writeln("Heya! I fixed your name, you're welcome, ", 
+    sortUsername(), "</br>");
