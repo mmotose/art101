d4176904efca2a936a81d438aea668eb759b7e10
mmotose <129877604+mmotose@users.noreply.github.com>
2023-05-02 15:19:50 -0700

    Update lab.js

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -2,20 +2,3 @@
 // Date: 5/1/2023
 
 // Constants
-myTransport = ["Toyota RAV4", "Mountain Bike"];
-
-myMainRide = {
-    make: "Toyota",
-    model: "RAV4",
-    color: "Blizzard Pearl",
-    year: 2006,
-    
-    age : function(){
-        return 2023 - this.year;
-    }
-}
-
-document.writeln("Getting Around: ", myTransport, "<br>");
-document.writeln("My Main Ride: <pre>",
-    JSON.stringify(myMainRide, null, '\t'), "</pre>");
-    
