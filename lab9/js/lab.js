f422f32f391076b15b32021922ed3e5ea1b97927
mmotose <129877604+mmotose@users.noreply.github.com>
2023-05-10 21:02:16 -0700

    Lab3

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -9,16 +9,20 @@ var outputEl = document.getElementById("output");
 //create new p elements using createElement("p")
 var new1El = document.createElement("p");
 var new2El = document.createElement("p");
+var new3El = document.createElement("p");
 
 //set innerHTML content of new1El and new2El to strings
 new1El.innerHTML = "Panguins are funny.";
 new2El.innerHTML = "Cheese cup of blue.";
+new3El.innerHTML = "HAHA I'm a wildcard";
 
 //append to child node of outputEl
 //display in output div
 outputEl.appendChild(new1El);
 outputEl.appendChild(new2El);
+outputEl.insertBefore(new3El, new1El);
 
+output
 //decorate
 new1El.style.color = "yellow";
 new1El.style.fontFamily = "Chiller";
@@ -26,6 +30,9 @@ new1El.style.fontFamily = "Chiller";
 new2El.style.color = "blue";
 new2El.style.fontFamily = "Comic Sans MS";
 
+new3El.style.color = "black";
+new3El.style.fontSize = "40px";
+new3El.style.fontFamily = "Marker Felt";
 
 /*
 You can also edit specifics with the text by using "style". For example with my code (pls don't copy and edit yourself): 
