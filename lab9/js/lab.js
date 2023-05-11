a85b24e524e563687c4c08f4061c8d77c8cfa501
mmotose <129877604+mmotose@users.noreply.github.com>
2023-05-10 20:41:07 -0700

    Lab9

---
+++ lab9/js/lab.js
@@ -0,0 +1,48 @@
+// Author: Miyuki Motose
+// Date: 5/10/2023
+
+// Constants
+
+//assign outputEl to "output"
+var outputEl = document.getElementById("output");
+
+//create new p elements using createElement("p")
+var new1El = document.createElement("p");
+var new2El = document.createElement("p");
+
+//set innerHTML content of new1El and new2El to strings
+new1El.innerHTML = "Panguins are funny.";
+new2El.innerHTML = "Cheese cup of blue.";
+
+//append to child node of outputEl
+//display in output div
+outputEl.appendChild(new1El);
+outputEl.appendChild(new2El);
+
+//decorate
+new1El.style.color = "yellow";
+new1El.style.fontFamily = "Chiller";
+
+new2El.style.color = "blue";
+new2El.style.fontFamily = "Comic Sans MS";
+
+
+/*
+You can also edit specifics with the text by using "style". For example with my code (pls don't copy and edit yourself): 
+// Add styles to the first <p> element
+new1El.style.color = "blue";
+new1El.style.textDecoration = "underline";
+
+// Add styles to the second <p> element
+new2El.style.color = "purple";
+new2El.style.fontStyle = "italic";
+
+// Create a new <p> element
+var new3El = document.createElement("p");
+// Set the inner HTML content of the third <p> element
+new3El.innerHTML = "Hello World!";
+
+// Add styles to the third <p> element
+new3El.style.color = "yellow";
+new3El.style.fontWeight = "bold";
+*/
