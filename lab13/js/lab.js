50b70e98ffd722e9461011d3579bca03673036a2
mmotose <129877604+mmotose@users.noreply.github.com>
2023-05-24 21:40:59 -0700

    Lab13 extra

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,11 +1,12 @@
 // Author: Miyuki Motose
 // Date: 5/24/2023
 
+
 var oneLongString = "";
 
-function fizzBuzzBoom(){
+function fizzBuzzBoom(maxNums){
   
-  for (i=1; i<=200; i++) {
+  for (i=1; i<=maxNums; i++) {
     
 // If i is divisible by certain numbers, output certain lines
     if (i%105 == 0) {
@@ -26,51 +27,14 @@ function fizzBuzzBoom(){
       oneLongString += i + "<br>";
     }
   }
-  return oneLongString;
+  
+  var styledOutput = '<span style="color: pink;">' + oneLongString + '</span>';
+  return styledOutput;
   
 }
 
 
-$("#output").append(fizzBuzzBoom());
-
-
-
-
-
-
-
-
-// var oneLongString = "";
-
-// function fizzBuzzBoom(maxNums){
-  
-//   for (i=1; i<=maxNums; i++) {
-    
-// // If i is divisible by certain numbers, output certain lines
-//     if (i%105 == 0) {
-//       oneLongString += i + " - FizzBuzzBoom!<br>";
-//     }else if (i%35== 0) {
-//       oneLongString += i + " - BuzzBoom!<br>";
-//     }else if (i%21 == 0) {
-//       oneLongString += i + " - FizzBoom!<br>";
-//     }else if (i%15 == 0) {
-//       oneLongString += i + " - FizzBuzz!<br>";
-//     }else if (i%7 == 0) {
-//       oneLongString += i + " - Boom!<br>";
-//     }else if (i%5 == 0) {
-//       oneLongString += i + " - Buzz!<br>";
-//     }else if (i%3 == 0) {
-//       oneLongString += i + " - Fizz!<br>";
-//     } else {
-//       oneLongString += i + "<br>";
-//     }
-//   }
-//   return oneLongString;
-  
-// }
-
-
-// $("#button").click(function(){
-//   var max = $("#input").val();
-//   $("#output").append(fizzBuzzBoom(max));
-// });
+$("#button").click(function(){
+  var max = $("#input").val();
+  $("#output").append(fizzBuzzBoom(max));
+});
